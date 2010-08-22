954b58b02bb0b9a26c365fa0772fe2c71f03cdce
Peter Donald <peter@realityforge.org>
2010-08-22 20:38:29 +1000

    Make sure that the jar processing occurs after the jar has been created by using the enhance in an enhance trick

--- lib/buildr/ipojo/project_extension.rb
+++ lib/buildr/ipojo/project_extension.rb
@@ -20,21 +20,24 @@ module Buildr
           project.packages.each do |pkg|
             if pkg.respond_to?(:to_hash) && pkg.to_hash[:type] == :jar
               pkg.enhance do
-                begin
-                  tmp_filename = pkg.to_s + ".out"
-                  metadata_file = project.ipojo.metadata_file
-                  if metadata_file.nil?
-                    metadata_file = project._(:target, :generated, :config, "ipojo.xml")
-                    mkdir_p File.dirname(metadata_file)
-                    File.open(metadata_file, "w") do |f|
-                      f << "<ipojo></ipojo>"
+                pkg.enhance do
+                  begin
+                    tmp_filename = pkg.to_s + ".out"
+                    metadata_file = project.ipojo.metadata_file
+                    if metadata_file.nil?
+                      metadata_file = project._(:target, :generated, :config, "ipojo.xml")
+                      mkdir_p File.dirname(metadata_file)
+                      File.open(metadata_file, "w") do |f|
+                        f << "<ipojo></ipojo>"
+                      end
                     end
+                    info("Processing #{File.basename(pkg.to_s)} through iPojo pre-processor")
+                    Buildr::Ipojo.pojoize(project, pkg.to_s, tmp_filename, metadata_file)
+                    FileUtils.mv tmp_filename, pkg.to_s
+                  rescue => e
+                    FileUtils.rm_rf pkg.to_s
+                    raise e
                   end
-                  Buildr::Ipojo.pojoize(project, pkg.to_s, tmp_filename, metadata_file)
-                  FileUtils.mv tmp_filename, pkg.to_s
-                rescue => e
-                  FileUtils.rm_rf pkg.to_s
-                  raise e
                 end
               end
             end
