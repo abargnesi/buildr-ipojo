5e47b21650577fe23cd128896b490ac1cfd2dc75
Peter Donald <peter@realityforge.org>
2010-08-22 14:16:32 +1000

    Remove all the necessary cruft from the spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,29 +16,17 @@ unless File.exist?("#{BUILDR_DIR}/buildr.gemspec")
   raise "Unable to find buildr.gemspec in #{BUILDR_DIR == DEFAULT_BUILDR_DIR ? 'guessed' : 'specified'} $BUILDR_DIR (#{BUILDR_DIR})"
 end
 
-require 'rubygems'
-
-# For testing we use the gem requirements specified on the buildr.gemspec
-Gem::Specification.load(File.expand_path("#{BUILDR_DIR}/buildr.gemspec", File.dirname(__FILE__))).
-  dependencies.select{|dep| dep.type == :runtime}.each do |dep|
-  gem dep.name, dep.requirement.to_s
-end
-
 # hook into buildr's spec_helpers load process
 module SandboxHook
   def SandboxHook.included(spec_helpers)
     $LOAD_PATH.unshift(File.dirname(__FILE__))
     $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
     require 'buildr_ipojo'
-    # Download deps into real local dir
-    Buildr.application.instance_eval { @rakefile = File.expand_path('buildfile') }
-    Buildr::repositories.remote << Buildr::Ipojo.remote_repository
-    Buildr::Ipojo.requires.each do |spec|
-      a = Buildr.artifact(spec)
-      a.invoke
-    end
-    Buildr::repositories.remote = ["file://#{Buildr::repositories.local}"]
   end
 end
 
 require "#{BUILDR_DIR}/spec/spec_helpers.rb"
+
+# Download deps into real local dir
+Buildr::repositories.remote << Buildr::Ipojo.remote_repository
+Buildr::Ipojo.requires.each { |spec| artifact(spec).invoke }
